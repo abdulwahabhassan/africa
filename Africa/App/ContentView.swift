d6165ce0b96add8c75306918ef5adf31c308153d
Hassan Abdulwahab <devhassan.org@gmail.com>
2022-06-08 15:23:08 +0100

    advanced grid completed

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -10,21 +10,81 @@ struct ContentView: View {
     
     //MARK: - PROPERTIES
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    @State private var isGridViewActive: Bool = false
+    @State private var  gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 1)
+    @State private var gridColumn: Int = 1
+    
+    
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    CoverImageView()
-                        .frame(height: 300)
-                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0 ))
-                    
-                    ForEach(animals) { item in
-                        NavigationLink(destination: AnimalDetailView(animal: item), label: {AnimalListItemView(animal: item)})
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0 ))
+                        
+                        ForEach(animals) { item in
+                            NavigationLink(destination: AnimalDetailView(animal: item), label: {AnimalListItemView(animal: item)})
+                        } //: LOOP
+                    }//: LIST
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                } //: NAVLINK
+                            } //: LOOP
+                        }//: GRID
+                        .padding(10)
+                        .animation(.easeIn)
+                    } //: SCROLLVIEW
+                }//: CONDITION
+            } //GROUP
+            .navigationTitle("Africa")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar(content: {
+                ToolbarItem (placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        Button(action: {
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        
+                        Button(action: {
+                            
+                            haptics.impactOccurred()
+                            if (isGridViewActive) {
+                                if(gridColumn < 3) {
+                                    gridColumn += 1
+                                } else {
+                                    gridColumn = 1
+                                }
+                            }
+                            gridLayout = Array(repeating: GridItem(.flexible()), count: gridColumn)
+                            isGridViewActive = true
+                        }) {
+                            if (gridColumn == 1) {
+                                Image(systemName: "rectangle.grid.1x2" )
+                                    .font(.title2)
+                                    .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                                    .frame(width: 32, height: 32)
+                            } else {
+                                Image(systemName: "square.grid.\(gridColumn)x\(gridColumn)" )
+                                    .font(.title2)
+                                    .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                                    .frame(width: 32, height: 32)
+                            }
+                        }
                     }
-                } //: LIST
-                .navigationTitle("Africa")
-                .navigationBarTitleDisplayMode(.large)
-            }
+                }
+            })
         } //: NAVIGATION
         .padding(.horizontal, -10)
         
--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State var selectedAnimal: String = "lion"
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    //describes a gridlayout with three columns to be created
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    //efficient grid definition
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    //Dynamic grid layout
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.white, lineWidth: 4)
+                    )
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: {value in
+                        gridSwitch()
+                    })
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.white, lineWidth: 1)
+                            )
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    }
+                } //: GRID
+                .animation(Animation.easeIn) //animate the grid
+                
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
+        } //: SCROLL
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
